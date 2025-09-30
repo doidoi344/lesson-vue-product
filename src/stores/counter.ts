943cb87ebf1f954ab3804594b5c438af328e76fe
doidoi344 <yuta.doi344@gmail.com>
2025-09-30 14:57:52 +0900

    add: pinia
    pinia のサンプルソースを追加

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -2,8 +2,11 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('counter', () => {
+  // piniaでは「state」と呼ぶ
   const count = ref(0)
+  // piniaでは「getter」と呼ぶ
   const doubleCount = computed(() => count.value * 2)
+  // piniaでは「action」と呼ぶ
   function increment() {
     count.value++
   }
--- src/views/SampleView.vue
+++ src/views/SampleView.vue
@@ -1,11 +1,16 @@
-script:
+<script setup lang="ts">
+import { useCounterStore } from '../stores/counter'
+const counterStore = useCounterStore()
+</script>
+
 <template>
   <div>
     <h1>サンプルページ</h1>
+    <div>現在のカウント数：{{ counterStore.count }}</div>
+    <div>倍にしたカウント数：{{ counterStore.doubleCount }}</div>
+    <button @click="counterStore.increment()">+1増加</button>
   </div>
 </template>
-
-<script setup lang="ts"></script>
 
 <style scoped>
 h1 {
